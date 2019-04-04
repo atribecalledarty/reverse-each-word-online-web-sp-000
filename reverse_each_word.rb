4e18926cfb493d0155811d513ccc1658da7aac0f
Arthur Song <arth3rs0ng@gmail.com>
2019-04-04 15:28:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   array = sentence.split(" ")
-  new_sentence = ""
+  new_array = []
   array.each do |word|
-    new_sentence += word.reverse
+    new_array << word.reverse
   end
-  new_sentence
+  new_array.join(" ")
 end
